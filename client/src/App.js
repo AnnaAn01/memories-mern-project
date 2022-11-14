1ae64c8ea08eea730bd8b9fffae90a54b0f9ab15
Anna An <arzumanyan.an@gmail.com>
2022-11-14 12:21:12 +0400

    adding Form, Posts, Post, and css files

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,24 @@
 import React from "react";
+import memories from "./images/memories.png";
+import Form from "./components/Form/Form";
+import Posts from "./components/Posts/Posts";
+import "./App.css";
 
 const App = () => {
   return (
-    <div>
-      <h1>App</h1>
+    <div className="container">
+      <nav className="navbar__container">
+        <h2 className="app__title">Memories</h2>
+        <img src={memories} alt="memories" className="app__image" />
+      </nav>
+      <div className="form-posts__container">
+        <div className="container__grid-posts">
+          <Posts />
+        </div>
+        <div className="container__grid-form">
+          <Form />
+        </div>
+      </div>
     </div>
   );
 };
---
+++ client/src/components/Form/Form.js
@@ -0,0 +1,8 @@
+import React from "react";
+import "./Form.css";
+
+const Form = () => {
+  return <h1>FORM</h1>;
+};
+
+export default Form;
