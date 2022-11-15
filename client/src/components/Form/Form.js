6c14193829e8d2bca84182e4f312a66e83c167f5
Anna An <arzumanyan.an@gmail.com>
2022-11-15 15:09:49 +0400

    creating the form on front end

--- client/src/components/Form/Form.js
+++ client/src/components/Form/Form.js
@@ -1,8 +1,104 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Form.css";
+import FileBase from "react-file-base64";
 
 const Form = () => {
-  return <h1>FORM</h1>;
+  const [postData, setPostData] = useState({
+    creator: "",
+    title: "",
+    message: "",
+    tags: "",
+    selectedFile: "",
+  });
+  const handleSubmit = () => {};
+
+  const clear = () => {};
+
+  return (
+    <>
+      <div className="form__paper">
+        <h6 className="form__title">Creating a Memory</h6>
+        <form
+          autoComplete="off"
+          noValidate
+          className="form__form"
+          onSubmit={handleSubmit}
+        >
+          <div className="input-group">
+            <label htmlFor="creator" name="creator" className="input-label">
+              Creator
+            </label>
+            <input
+              className="form__input"
+              type="text"
+              name="creator"
+              value={postData.creator}
+              onChange={(e) =>
+                setPostData({ ...postData, creator: e.target.value })
+              }
+            />
+          </div>
+          <div className="input-group">
+            <label htmlFor="title" name="title" className="input-label">
+              Title
+            </label>
+            <input
+              className="form__input"
+              type="text"
+              name="title"
+              value={postData.title}
+              onChange={(e) =>
+                setPostData({ ...postData, title: e.target.value })
+              }
+            />
+          </div>
+          <div className="input-group">
+            <label htmlFor="message" name="message" className="input-label">
+              Message
+            </label>
+            <input
+              className="form__input"
+              type="text"
+              name="message"
+              value={postData.message}
+              onChange={(e) =>
+                setPostData({ ...postData, message: e.target.value })
+              }
+            />
+          </div>
+          <div className="input-group">
+            <label htmlFor="tags" name="tags" className="input-label">
+              Tags
+            </label>
+            <input
+              className="form__input"
+              type="text"
+              name="tags"
+              value={postData.tags}
+              onChange={(e) =>
+                setPostData({ ...postData, tags: e.target.value })
+              }
+            />
+          </div>
+          <div className="form__fileInput">
+            <FileBase
+              type="file"
+              multiple={false}
+              onDone={({ base64 }) =>
+                setPostData({ ...postData, selectedFile: base64 })
+              }
+            />
+          </div>
+          <button type="submit" className="form__buttonSubmit btn1">
+            Submit
+          </button>
+          <button onClick={clear} className="form__buttonSubmit btn2">
+            Clear
+          </button>
+        </form>
+      </div>
+    </>
+  );
 };
 
 export default Form;
