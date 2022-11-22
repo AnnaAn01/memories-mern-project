82581cb5edd0705c45b2d8afc8e629be57c5b4c9
Anna An <arzumanyan.an@gmail.com>
2022-11-22 18:33:51 +0400

    refactoring

--- client/src/actions/posts.js
+++ client/src/actions/posts.js
@@ -7,22 +7,6 @@ import {
   LIKE,
 } from "../constants/actionTypes";
 
-// Action creators
-// redux thunk lets us use the below syntax "async (dispatch) =>". We use 2 arrows
-// export const getPosts = () => async (dispatch) => {
-//   try {
-//     // trying to fetch all the data from the api
-//     const { data } = await api.fetchPosts();
-//     dispatch({ type: "FETCH_ALL", payload: data });
-//   } catch (error) {
-//     console.log(error.message);
-//   }
-
-//   //   const action = { type: "FETCH_ALL", payload: [] };
-//   //   with redux thunk, instead of return, we use dispatch
-//   //   return action;
-// };
-
 export const getPosts = () => async (dispatch) => {
   try {
     const { data } = await api.fetchPosts();
