26b0652d0c2d59c996364c52ffaa84a7a2e241c8
Anna An <arzumanyan.an@gmail.com>
2022-12-01 10:33:39 +0400

    ading env variables

--- client/src/reducers/posts.js
+++ client/src/reducers/posts.js
@@ -1,16 +1,23 @@
+import {
+  FETCH_ALL,
+  CREATE,
+  UPDATE,
+  DELETE,
+  LIKE,
+} from "../constants/actionTypes";
+
 // our state in this app is going to be posts, so we'll rename state to post here
 const reducers = (posts = [], action) => {
   switch (action.type) {
-    case "DELETE":
+    case DELETE:
       return posts.filter((post) => post._id !== action.payload);
 
-    case "FETCH_ALL":
+    case FETCH_ALL:
       return action.payload;
-    case "CREATE":
+    case CREATE:
       return [...posts, action.payload];
     // putting two cases below each other will do the same thing for both
-    case "UPDATE":
-    case "LIKE":
+    case UPDATE:
       return posts.map((post) =>
         post._id === action.payload._id ? action.payload : post
       );
